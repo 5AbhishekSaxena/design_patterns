7db252d33a119a4d1562021ae861f4d1da72ea0a
5abhisheksaxena <5abhisheksaxena@gmail.com>
2021-02-22 16:05:40 +0530

    Add observer pattern

---
+++ src/b_observer_pattern/observers/DesktopDisplay.kt
@@ -0,0 +1,20 @@
+package b_observer_pattern.observers
+
+import b_observer_pattern.observables.WeatherStation
+
+
+/**
+ * Created by Abhishek Saxena on 22-02-2021.
+ */
+
+class DesktopDisplay(
+    private val weatherStation: WeatherStation
+) : IObserver {
+    override fun update() {
+        println(
+            "desktop display update, temperature: ${
+                weatherStation.temperature
+            }"
+        )
+    }
+}
---
+++ src/b_observer_pattern/observers/IObserver.kt
@@ -0,0 +1,10 @@
+package b_observer_pattern.observers
+
+
+/**
+ * Created by Abhishek Saxena on 22-02-2021.
+ */
+
+interface IObserver {
+    fun update()
+}
