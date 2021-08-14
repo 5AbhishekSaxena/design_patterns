b4cca3a90134531014d2f0770f078432b5a9e6d9
5abhisheksaxena <5abhisheksaxena@gmail.com>
2021-08-14 15:38:29 +0530

    Add documentation for UiKitFactoryProvider

--- src/tech/developingdeveloper/e_abstract_factory_pattern/base/UiKitFactoryProvider.kt
+++ src/tech/developingdeveloper/e_abstract_factory_pattern/base/UiKitFactoryProvider.kt
@@ -8,8 +8,18 @@ import tech.developingdeveloper.e_abstract_factory_pattern.windows.WindowsUiKitF
  * Created by Abhishek Saxena on 14-08-2021.
  */
 
+/**
+ * Helper class to create [UiKitFactory], use [createUiKitFactory] method to create an instance based on the type.
+ */
 object UiKitFactoryProvider {
 
+    /**
+     * A static method to create UiKitFactory using [type].
+     *
+     * @param type defines the type of factory to create.
+     *
+     * @throws IllegalArgumentException when the type doesn't match with any of the given factory
+     */
     fun createUiKitFactory(type: String): UiKitFactory {
         return when {
             type.equals("Mac", ignoreCase = true) -> MacUiKitFactory()
